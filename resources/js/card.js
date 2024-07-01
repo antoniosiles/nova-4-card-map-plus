bfd8a0a34c450ea791915aee4d9ee6908414ab2e
Antonio Siles <asiles@siberian.com.ec>
2024-07-01 15:37:33 -0400

    dibujar marcadores desde un controlador mediante un endpoint, part 3, add axios import

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,5 +1,9 @@
+import axios from 'axios';
 import Card from './components/Card'
 
 Nova.booting((app, store) => {
   app.component('nova-4-card-map-plus', Card)
 })
+app.axios = axios;
+app.$http = axios;
+window.axios = axios
